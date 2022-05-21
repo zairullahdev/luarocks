6874c459840e09d7c8fd5f930eef02d29a8cc328
zairullahdev <99532718+zairullahdev@users.noreply.github.com>
2022-05-21 21:43:24 +0700

    Create termuxsetup.sh

---
+++ termuxsetup.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+
+apt-get install figlet > /dev/null
+
+figlet "Luarocks port for Termux"
+
+echo "Installing Lua 5.4"
+
+apt-get install lua54 > /dev/null
+
+echo "Installing Git"
+
+apt-get install git > /dev/null
+
+echo "Cloning"
+
+git clone https://github.com/zairullahdev/luarocks
+
+echo "Setup is just for second, not too long"
+
+cd luarocks && ./configure && make && make install
+
+echo "Removing Cache's....."
+
+cd ~ && rm -rf luarocks
+
+echo "To test it, run luarocks"
